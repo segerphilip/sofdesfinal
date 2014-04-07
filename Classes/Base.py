bc7f87dd9796e0b83b3c5f3785aa4d1954dece22
Shrinidhi Thirumalai <shrinidhit@Shrinidhi.(none)>
2014-04-06 21:17:36 -0400

    Adding Base and Room

--- Classes/Base.py
+++ Classes/Base.py
@@ -1,3 +1,13 @@
 class Base(Room):#generates the main base, standard for every game. Similar to Room
-  def generate_room():
-    pass
+    def generate_room(self, screenWidth, screenHeight):
+        #Initializing variables
+        self.roomMap = {}
+        self.roomItems = []
+        #Settings
+        boxSize = 50 #pixel width of each box in the grid
+        characterNum = 5
+        #For loop to create roomMap
+        for i in range (0, characterNum):
+            x = random([range(0, screenWidth/boxSize)])
+            y = random([range(0, screenHeight/boxSize)]
+            roomMap.update({(x,y): Character(characterImage, x, y)})
--- Classes/Room.py
+++ Classes/Room.py
@@ -1,7 +1,35 @@
+import random
+
 class Room():#generatesgame rooms, where players will be transported depending on decisions 
-  def __init__(self):
-    pass
-  def generate_room():
-    pass
-  def render_room():
+  def __init__(self, screenWidth, screenHeight):
+    generate_room(self, screenWidth, screenHeight)
+  def generate_room(self, screenWidth, screenHeight):
+    #Initializing variables
+    self.roomMap = {}
+    self.roomItems = []
+    enemyNumber = 0;
+    itemNumber = 0;
+    #Settings
+    options = ['Enemy', 'Item']
+    objectProbability = .1
+    boxSize = 50; #pixel width of each box in the grid
+    #For loop to create roomMap
+    for x in range(0, screenWidth, boxSize):
+        for y in range(0, screenHeight, boxSize):
+            #decides if object will be placed(probability)
+            if random.choice([range(0, 100)]) <= (objectProbability * 100):
+                objectType = random.choice(options) #creates object type
+                #if object type is enemy: place the object in the dictionary, and save it to the list
+                if objectType == 'Enemy':
+                    enemynumber += 1
+                    'Enemy' + str(enemyNumber) = Enemy(enemyImage, x, y)
+                    roomMap.update({(x,y): 'Enemy' + str(enemyNumber)})
+                    roomItems.append('Enemy' + str(enemyNumber))
+                #if object type is item: place the object in the dictionary, and save it to the list
+                elif objectType == 'Item':
+                    itemNumber += 1
+                    'Item' + str(itemNumber) = Item(itemImage, x, y)
+                    roomMap.update({(x,y): Item(image, x, y)})
+                    roomItems.append('Item' + str(itemNumber))
+  def render_room(self):
     pass
